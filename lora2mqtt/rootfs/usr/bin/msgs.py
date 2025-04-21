f273568a307c3ce72e84b1b0572fc460fe4783b1
Leonardo Figueiro <leoagfig@gmail.com>
2025-04-21 19:40:36 -0300

    Update msgs.py

--- lora2mqtt/rootfs/usr/bin/msgs.py
+++ lora2mqtt/rootfs/usr/bin/msgs.py
@@ -542,8 +542,8 @@ def lora_set_modo_op(modo_op):
     if modo_op == MODE_OP_PAIRING:
         cmdUsb = CMD_SET_SYNCH_WORD_PAIRING
     globals.g_serial.write(cmdUsb.encode('utf-8'))    # Enviando uma string (precisa ser em bytes)
-    logging.info(f"Enviado comando muda synch_word: {cmdUsb}")
-    time.sleep(2)  # Aguarda 2 segundos
+    logging.debug(f"Enviado comando muda synch_word: {cmdUsb}")
+    time.sleep(2)  # Aguarda 2 segundos e envia novamente
     globals.g_serial.write(cmdUsb.encode('utf-8'))    # Enviando uma string (precisa ser em bytes)
     globals.g_lf_lora.set_modo_op(modo_op)
 
